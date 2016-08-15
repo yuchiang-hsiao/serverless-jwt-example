ee75bfc856440ed9d916e18b0a369ca874298650
Hsiao Yu Chiang <chung76s@gmail.com>
2016-08-15 21:28:07 +0800

    add login and check

--- check/handler.js
+++ check/handler.js
@@ -1,7 +1,18 @@
 'use strict';
+var jwt = require('jsonwebtoken');
+var secretOrPublicKey = 'SECRET';
 
-module.exports.handler = function(event, context, cb) {
-  return cb(null, {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
-  });
+module.exports.handler = function(event, context) {
+    if (event.token) {
+        try {
+            var decode = jwt.verify(event.token, secretOrPublicKey);
+        } catch (err) {
+            console.log(err)
+        }
+        if (decode) {
+            context.succeed({status: true})
+        } else {
+            context.fail({status: false})
+        }
+    }
 };
--- login/handler.js
+++ login/handler.js
@@ -1,7 +1,20 @@
 'use strict';
+var jwt = require('jsonwebtoken');
+var PASSWORD = 'password';
+var secretOrPrivateKey = 'SECRET';
 
-module.exports.handler = function(event, context, cb) {
-  return cb(null, {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
-  });
+module.exports.handler = function(event, context) {
+    console.log(event);
+    if (event.body.password && event.body.password === PASSWORD) {
+        var dateTime = new Date().getTime();
+        var timestamp = Math.floor(dateTime / 1000) + (60 * 60 * 24 * 7); // 一週後失效
+        var payload = {
+            iss: 'mysite.com',
+            exp: timestamp
+        }
+        var token = jwt.sign(payload, secretOrPrivateKey);
+        context.succeed({token: token});
+    } else {
+        context.fail({status: 'fail'})
+    }
 };
